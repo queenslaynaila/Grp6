3b7566cd0b481d98dc5bc8735e2949a43ef8e6f9
Queenslay Jema <queenslay.naila@student.moringaschool.com>
2022-09-21 12:33:15 +0300

    Add bootstrap

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './index.css'; 
 import React from "react";
 import ReactDOM  from "react-dom";
 import App from "./Components/App"
-import './index.css';
+
  
 
 
