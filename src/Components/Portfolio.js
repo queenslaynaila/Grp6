4e9bec768c5fb5a08627647760c0b7addf51085d
Queenslay Jema <queenslay.naila@student.moringaschool.com>
2022-09-21 11:12:10 +0300

    services

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -30,7 +30,7 @@ function Portfolio(){
         <p>Yes, San Fran is ours</p>
       </div>
     </div>
-  </div><br>
+  </div><br></br>
   
   <h2>What our customers say</h2>
   <div id="myCarousel"  className="carousel slide text-center" data-ride="carousel">
@@ -44,13 +44,13 @@ function Portfolio(){
    
     <div  className="carousel-inner" role="listbox">
       <div  className="item active">
-        <h4>"This company is the best. I am so happy with the result!"<br><span>Michael Roe, Vice President, Comment Box</span></h4>
+        <h4>"This company is the best. I am so happy with the result!"<br></br><span>Michael Roe, Vice President, Comment Box</span></h4>
       </div>
       <div  className="item">
-        <h4>"One word... WOW!!"<br><span>John Doe, Salesman, Rep Inc</span></h4>
+        <h4>"One word... WOW!!"<br></br><span>John Doe, Salesman, Rep Inc</span></h4>
       </div>
       <div  className="item">
-        <h4>"Could I... BE any more happy with this company?"<br><span>Chandler Bing, Actor, FriendsAlot</span></h4>
+        <h4>"Could I... BE any more happy with this company?"<br></br><span>Chandler Bing, Actor, FriendsAlot</span></h4>
       </div>
     </div>
 
