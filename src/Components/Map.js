ae4c9bf8fbd44c83de15f3f391c3072bcafb6dbb
Queenslay Jema <queenslay.naila@student.moringaschool.com>
2022-09-22 10:48:23 +0300

    deploy to gh-pages

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -8,9 +8,9 @@ function Map(){
 
 function Footer(){
     return(
-        <footer class="container-fluid text-center">
+        <footer className="container-fluid text-center">
         <a href="#myPage" title="To Top">
-          <span class="glyphicon glyphicon-chevron-up"></span>
+          <span className="glyphicon glyphicon-chevron-up"></span>
         </a>
         <p>Bootstrap Theme Made By <a href="https://www.w3schools.com" title="Visit w3schools">www.w3schools.com</a></p>
       </footer>
